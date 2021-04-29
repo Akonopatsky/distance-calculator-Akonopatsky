ba3c8c679a0498ce61efa5de702350f49900374b
Akonopatsky <Akonopatsky@gmail.com>
2021-04-29 16:16:48 +0300

    Empty project. Come dependencies are commented.

---
+++ src/main/java/distance_calculator/DistanceCalculator.java
@@ -0,0 +1,13 @@
+package distance_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceCalculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceCalculator.class, args);
+	}
+
+}
